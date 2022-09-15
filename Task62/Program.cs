d63b6bdcec8dfe0e5cfd50eca020df3ed8074066
Vera Krasulina <kshisinka@hotmail.com>
2022-09-15 10:18:47 +0300

    Task 62

---
+++ Task62/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+
+int[,] SpiralMassive(int side)
+{
+    int[,] spiralMassive = new int[side, side];
+    int value = 0;
+    int count = side;
+    int vol = -side;
+    int ind = -1;
+    while (count > 0)
+    {
+        vol = -1 * vol / side;
+        for (int i = 0; i < count; i++)
+        {
+            ind += vol;
+            spiralMassive[ind / side, ind % side] = value++;
+        }
+        vol *= side;
+        count--;
+        for (int i = 0; i < count; i++)
+        {
+            ind += vol;
+            spiralMassive[ind / side, ind % side] = value++;
+        }
+    }
+    return spiralMassive;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int x = 4;
+int[,] arrey = SpiralMassive(x);
+PrintArray(arrey);
